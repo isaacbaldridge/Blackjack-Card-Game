a910f001af8444b48b82ca4c1901e9cf101a95a8
isaacbaldridge <baldridgela@alumni.vcu>
2023-11-22 12:58:02 -0500

    refactored code for obtaining dealt card info

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import random
 cards = []
 suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+ranks = [
+    {"rank": "A", "value": 11},
+    {"rank": "2", "value": 2},
+    {"rank": "3", "value": 3},
+    {"rank": "4", "value": 4},
+    {"rank": "5", "value": 5},
+    {"rank": "6", "value": 6},
+    {"rank": "7", "value": 7},
+    {"rank": "8", "value": 8},
+    {"rank": "9", "value": 9},
+    {"rank": "10", "value": 10},
+    {"rank": "J", "value": 10},
+    {"rank": "Q", "value": 10},
+    {"rank": "K", "value": 10}
+]
 
 for suit in suits:
     for rank in ranks:
@@ -20,16 +34,47 @@ def deal(number):
     return cardsDealt
 
 shuffle()
-cardsDealt = deal(2)
-card = cardsDealt[0]
-rank = card[1]
 
-if rank == "A":
-    value = 11
-elif rank == "J" or rank == "Q" or rank == "K":
-    value = 10
-else:
-    value = rank
+card = deal(1)[0]
+
+print(card[1]["value"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cardsDealt = deal(2)
+# print(cardsDealt)
+# card = cardsDealt[0]
+# rank = card[1]
+
+# if rank == "A":
+#     value = 11
+# elif rank == "J" or rank == "Q" or rank == "K":
+#     value = 10
+# else:
+#     value = rank
     
 # print(card)
-print(rank, value)
+
+# rank_dict = {
+#     "rank": rank,
+#     "value": value 
+# }
+
+# print(rank_dict["rank"], rank_dict["value"])
